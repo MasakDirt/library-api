8ee2feb2d6866eab4afa4b0390bd976cbf7868e2
maksym_korniev <maksymkorniev88@gmail.com>
2024-10-16 21:24:51 +0300

    add book view set to urls

--- books/urls.py
+++ books/urls.py
@@ -1,5 +1,14 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from books.views import BookViewSet
+
+
 app_name = "books"
 
-urlpatterns = [
+router = DefaultRouter()
+router.register("books", BookViewSet, basename="book")
 
+urlpatterns = [
+    path("", include(router.urls))
 ]
