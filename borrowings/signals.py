99e47cf993aa0017b547ae91cbc167117aa55a3b
maksym_korniev <maksymkorniev88@gmail.com>
2024-10-17 22:36:28 +0300

    create signal for checking when Borrowing is created

---
+++ borrowings/signals.py
@@ -0,0 +1,20 @@
+import asyncio
+from typing import Type
+
+from django.db.models import Model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from borrowings.models import Borrowing
+from telegram_bot.notify import notify_borrowing_create
+
+
+@receiver(post_save, sender=Borrowing)
+def borrowing_created(
+        sender: Type[Model],
+        instance: Borrowing,
+        created: bool,
+        **kwargs
+) -> None:
+    if created:
+        asyncio.run(notify_borrowing_create(instance))
