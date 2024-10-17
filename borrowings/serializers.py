534668d5ce81bece878f007c910968ef25471c9e
Myroslava Nezbrytska <miroslavanezbricka@gmail.com>
2024-10-17 12:01:48 +0300

    "register Borrowing in admin and added serializers.py"

---
+++ borrowings/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import Borrowing
+from books.serializers import BookSerializer
+
+class BorrowingReadSerializer(serializers.ModelSerializer):
+    book = BookSerializer(read_only=True)
+
+    class Meta:
+        model = Borrowing
+        fields = [
+            "id",
+            "user",
+            "book",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date"
+        ]
+
+class BorrowingWriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = ["book", "expected_return_date", "actual_return_date"]
