32962bac4aa23373d2c956eb6c8ffaea78834e8e
maksym_korniev <maksymkorniev88@gmail.com>
2024-10-18 01:39:28 +0300

    add test for checking if notify method called, and add mock.patches to tests which create mocks

--- borrowings/tests/test_borrowing_api.py
+++ borrowings/tests/test_borrowing_api.py
@@ -1,4 +1,6 @@
+from datetime import date, timedelta
 from decimal import Decimal
+from unittest.mock import patch
 
 from django.contrib.auth import get_user_model
 from django.test import TestCase
@@ -8,9 +10,6 @@ from rest_framework.test import APIClient
 
 from books.models import Book
 from borrowings.models import Borrowing
-from datetime import date, timedelta, datetime
-
-from borrowings.serializers import BorrowingReadAuthenticatedSerializer
 
 
 BORROWINGS_URL = reverse("borrowings:borrowings-list")
@@ -19,8 +18,10 @@ BORROWINGS_URL = reverse("borrowings:borrowings-list")
 def get_detail(borrowings_id: int) -> str:
     return reverse("borrowings:borrowings-detail", args=[borrowings_id])
 
+
 def sample_user(email, password):
     return get_user_model().objects.create_user(email, password)
+
 
 def sample_book(**additional) -> Book:
     defaults = {
@@ -33,6 +34,7 @@ def sample_book(**additional) -> Book:
     defaults.update(additional)
 
     return Book.objects.create(**defaults)
+
 
 def sample_borrowing(user, book):
     return Borrowing.objects.create(
@@ -58,7 +60,8 @@ class BorrowingTests(TestCase):
         self.client = APIClient()
         self.client.force_authenticate(self.user)
 
-    def test_create_borrowing(self):
+    @patch("borrowings.signals.notify_borrowing_create")
+    def test_create_borrowing(self, mocked_notify):
         data = {
             "book": self.book.id,
             "expected_return_date": (date.today() +
@@ -69,7 +72,8 @@ class BorrowingTests(TestCase):
         self.assertEqual(Borrowing.objects.count(), 1)
         self.assertEqual(Borrowing.objects.first().user, self.user)
 
-    def test_list_borrowings(self):
+    @patch("borrowings.signals.notify_borrowing_create")
+    def test_list_borrowings(self, mocked_notify):
         Borrowing.objects.create(
             user=self.user,
             book=self.book,
@@ -80,7 +84,8 @@ class BorrowingTests(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data["results"]), 1)
 
-    def test_borrowing_detail(self):
+    @patch("borrowings.signals.notify_borrowing_create")
+    def test_borrowing_detail(self, mocked_notify):
         borrowing = Borrowing.objects.create(
             user=self.user,
             book=self.book,
@@ -91,7 +96,8 @@ class BorrowingTests(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["id"], borrowing.id)
 
-    def test_return_borrowing(self):
+    @patch("borrowings.signals.notify_borrowing_create")
+    def test_return_borrowing(self, mocked_notify):
         borrowing = Borrowing.objects.create(
             user=self.user, book=self.book,
             expected_return_date=date.today() + timedelta(days=7)
@@ -100,12 +106,17 @@ class BorrowingTests(TestCase):
         data = {
             "actual_return_date": date.today().isoformat(),
         }
-        response = self.client.patch(get_detail(borrowing.id), data, format="json")
+        response = self.client.patch(
+            get_detail(borrowing.id),
+            data,
+            format="json"
+        )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         borrowing.refresh_from_db()
         self.assertEqual(borrowing.actual_return_date, date.today())
 
-    def test_inventory_decrease_on_borrow(self):
+    @patch("borrowings.signals.notify_borrowing_create")
+    def test_inventory_decrease_on_borrow(self, mocked_notify):
         initial_inventory = self.book.inventory
         self.borrowing_data = {
             "book": self.book.id,
@@ -132,7 +143,8 @@ class UnAuthenticatedBorrowingTests(TestCase):
 
 
 class AuthenticatedBorrowingTests(TestCase):
-    def setUp(self):
+    @patch("borrowings.signals.notify_borrowing_create")
+    def setUp(self, mocked_notify):
         self.user = sample_user(
             email="test@test.com",
             password="password"
@@ -160,14 +172,45 @@ class AuthenticatedBorrowingTests(TestCase):
         response = self.client.get(get_detail(self.user.id))
         response_not_found = self.client.get(get_detail(self.user_2.id))
 
-        self.assertEqual(response_not_found.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(
+            response_not_found.status_code,
+            status.HTTP_404_NOT_FOUND
+        )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         self.assertIsNone(response.data.get("user"))
 
+    @patch("borrowings.signals.notify_borrowing_create")
+    def test_create_user_borrowings_and_notification_send(self, mocked_notify):
+        payload = {
+            "book": self.book.id,
+            "expected_return_date": date(2024, 12, 10),
+            "actual_return_date": date(2024, 12, 1),
+        }
+        response = self.client.post(
+            BORROWINGS_URL,
+            data=payload,
+            format="json"
+        )
+
+        borrowing = Borrowing.objects.get(pk=response.data["id"])
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(payload["book"], borrowing.book.id)
+        self.assertEqual(
+            payload["expected_return_date"],
+            borrowing.expected_return_date
+        )
+        self.assertEqual(
+            payload["actual_return_date"],
+            borrowing.actual_return_date
+        )
+        self.assertTrue(mocked_notify.called)
+
 
 class AdminBorrowingTests(TestCase):
-    def setUp(self):
+    @patch("borrowings.signals.notify_borrowing_create")
+    def setUp(self, mocked_notify):
         self.user = get_user_model().objects.create_superuser(
             email="test@test.com",
             password="password"
@@ -195,7 +238,8 @@ class AdminBorrowingTests(TestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_list_filter_is_active_borrowings(self):
+    @patch("borrowings.signals.notify_borrowing_create")
+    def test_list_filter_is_active_borrowings(self, mocked_notify):
         Borrowing.objects.create(
             user=self.user_2,
             book=self.book,
