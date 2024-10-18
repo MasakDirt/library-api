258c281eb09438d419b2e73e2c1f423d67dc7b82
maksym_korniev <maksymkorniev88@gmail.com>
2024-10-18 13:37:14 +0300

    add search filter to book view set and add documentation description about filtering

--- books/views.py
+++ books/views.py
@@ -1,6 +1,8 @@
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import viewsets
 from rest_framework.serializers import ModelSerializer
 
+from books.filters import CustomBookSearchFilter
 from books.models import Book
 from books.permissions import IsAdminOrReadOnly
 from books.serializers import (
@@ -14,9 +16,10 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = (IsAdminOrReadOnly,)
+    filter_backends = (CustomBookSearchFilter,)
 
     def get_serializer_class(self) -> ModelSerializer:
-        serializer = self.serializer_class
+        serializer = super().get_serializer_class()
 
         if self.action == "list":
             serializer = BookListSerializer
@@ -25,3 +28,23 @@ class BookViewSet(viewsets.ModelViewSet):
             serializer = BookRetrieveSerializer
 
         return serializer
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="title",
+                type=str,
+                description="Find book with this title",
+                enum=["Study", "St", "Free"]
+            ),
+            OpenApiParameter(
+                name="author",
+                type=str,
+                description="Find book with this author",
+                enum=["peter", "Ma"]
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        """List of books with pagination and searching"""
+        return super().list(request, *args, **kwargs)
