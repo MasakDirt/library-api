67d077fad2b7fdba548ae6155a99126098e98afa
maksym_korniev <maksymkorniev88@gmail.com>
2024-10-18 14:13:44 +0300

    refactor 2 tests into 1

--- books/tests/test_book_api.py
+++ books/tests/test_book_api.py
@@ -51,36 +51,27 @@ class UnAuthenticatedBookAPITests(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["results"], serializer.data)
 
-    def test_searching_book_by_author(self):
+    def test_filter_books(self):
         author = "Search"
-        sample_book(author=author)
+        title = "tes"
+        extra_book_title = sample_book(author=author)
         sample_book(title="Test2", author=author, cover="HARD")
         sample_book(title="Test3", inventory=11, author=author)
-        extra_book = sample_book(title="Test3", inventory=11)
-        author_search = author[:4]
+        extra_book_author = sample_book(title="Test3", inventory=11)
 
-        response = self.client.get(BOOK_URL, data={"author": author_search})
-        books = Book.objects.filter(author__icontains=author_search)
-        self.assertNotIn(extra_book, books)
-        serializer = BookListSerializer(books, many=True)
+        for filter_field, filter_value, extra_book in [
+            ("title", title, extra_book_title),
+            ("author", author[:4], extra_book_author),
+        ]:
+            response = self.client.get(BOOK_URL, {filter_field: filter_value})
+            books = Book.objects.filter(
+                **{f"{filter_field}__icontains": filter_value}
+            )
+            self.assertNotIn(extra_book, books)
+            serializer = BookListSerializer(books, many=True)
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data["results"], serializer.data)
-
-    def test_searching_book_by_title(self):
-        extra_book = sample_book()
-        sample_book(title="Test2",cover="HARD")
-        sample_book(title="Test3")
-        sample_book(title="Test3", inventory=11)
-        title_search = "tes"
-
-        response = self.client.get(BOOK_URL, data={"title": title_search})
-        books = Book.objects.filter(title__icontains=title_search)
-        self.assertNotIn(extra_book, books)
-        serializer = BookListSerializer(books, many=True)
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data["results"], serializer.data)
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            self.assertEqual(response.data["results"], serializer.data)
 
     def test_book_detail(self) -> None:
         book = sample_book()
