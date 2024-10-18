d76a6b50f16a9d87e90dde7271efdd997f7578a1
Myroslava Nezbrytska <miroslavanezbricka@gmail.com>
2024-10-18 12:23:13 +0300

    broken celery

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,5 @@
+from typing import Type
+
 from rest_framework import viewsets
 
 from .models import Borrowing
@@ -10,11 +12,12 @@ from .serializers import (
 class BorrowingViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.select_related("book", "user")
 
-    def get_serializer_class(self):
+    def get_serializer_class(self) -> Type[
+        BorrowingReadSerializer | BorrowingCreateSerializer
+        ]:
         if self.action in ["list", "retrieve"]:
             return BorrowingReadSerializer
         return BorrowingCreateSerializer
 
-    def perform_create(self, serializer):
+    def perform_create(self, serializer) -> None:
         serializer.save(user=self.request.user)
-
--- library_api/settings.py
+++ library_api/settings.py
@@ -43,11 +43,12 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "drf_spectacular",
+    "django_celery_beat",
   
     #  custom apps
     "books",
     "user",
-    "borrowings"
+    "borrowings",
 ]
 
 MIDDLEWARE = [
@@ -161,4 +162,17 @@ SIMPLE_JWT = {
     "ROTATE_REFRESH_TOKENS": False,
     "ALGORITHM": "HS512",
     "AUTH_HEADER_NAME": "HTTP_AUTHORIZE",
+}
+
+CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_TIMEZONE = "UTC"
+
+CELERY_BEAT_SCHEDULE = {
+    "check-overdue-borrowings-every-day": {
+        "task": "borrowings.tasks.check_overdue_borrowings",
+        "schedule": 86400.0,
+    },
 }
