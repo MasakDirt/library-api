84f139c1d38dba4e1e6f11c207b1154fef7f0c2d
maksym_korniev <maksymkorniev88@gmail.com>
2024-10-20 20:57:00 +0300

    refactor tests

--- library_api/settings.py
+++ library_api/settings.py
@@ -26,7 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = os.getenv(
+    "DJANGO_SECRET_KEY",
+    "m53$jj)lrttpi4we)nr_@5crjk5#72*=%a3l_kei)h0(g8je(@"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
