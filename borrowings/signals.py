cfdf1bd2ab3258217749ae6466a49c61d143bb80
maksym_korniev <maksymkorniev88@gmail.com>
2024-10-20 20:39:32 +0300

    make all notifications is sending asynchronously

--- borrowings/signals.py
+++ borrowings/signals.py
@@ -1,20 +1,37 @@
 import asyncio
 from typing import Type
 
+import httpx
 from django.db.models import Model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from borrowings.models import Borrowing
 from payments.utils import create_payment_with_session
-from telegram_bot.notify import notify_borrowing_create
 
 
 @receiver(post_save, sender=Borrowing)
 def borrowing_created(
-    sender: Type[Model], instance: Borrowing, created: bool, **kwargs
+    sender: Type[Model],
+    instance: Borrowing,
+    created: bool,
+    **kwargs
 ) -> None:
     if created:
-        asyncio.run(notify_borrowing_create(instance))
-
         create_payment_with_session(instance)
+
+        borrowing_data = {
+            "user_email": instance.user.email,
+            "book_title": instance.book.title,
+            "borrow_date": str(instance.borrow_date),
+            "expected_return_date": str(instance.expected_return_date)
+        }
+
+        async def send_notification():
+            async with httpx.AsyncClient() as client:
+                await client.post(
+                    "http://localhost:8001/notify/",
+                    json=borrowing_data
+                )
+
+        asyncio.run(send_notification())
