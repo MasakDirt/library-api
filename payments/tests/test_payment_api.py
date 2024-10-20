3711dbe6b98ac0d1cbebfd035db1f3f697a4664e
maksym_korniev <maksymkorniev88@gmail.com>
2024-10-20 17:05:33 +0300

    add tests for success and cancel url

--- payments/tests/test_payment_api.py
+++ payments/tests/test_payment_api.py
@@ -1,5 +1,8 @@
+import datetime
+from dataclasses import dataclass
 from unittest.mock import patch
 
+import stripe
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
@@ -8,6 +11,7 @@ from rest_framework.test import APIClient
 
 from books.models import Book
 from borrowings.models import Borrowing
+from payments.models import Payment
 from payments.serializers import (
     PaymentListSerializer,
     PaymentRetrieveSerializer,
@@ -24,10 +28,29 @@ def get_detail(payment_id: int) -> str:
     return reverse("payments:payment-detail", args=[payment_id])
 
 
+def get_success(payment_id: int) -> str:
+    return reverse("payments:payment-success", args=[payment_id])
+
+
+def get_cancel(payment_id: int) -> str:
+    return reverse("payments:payment-cancel", args=[payment_id])
+
+
+@dataclass
 class SessionStripe:
-    def __init__(self, id: str, url: str) -> None:
-        self.id = id
-        self.url = url
+    id: str
+    url: str
+
+
+@dataclass
+class CustomerDetailsTest:
+    name: str
+    email: str
+
+
+@dataclass
+class SuccessSessionTest:
+    customer_details: CustomerDetailsTest
 
 
 class PaymentAPITests(TestCase):
@@ -36,7 +59,8 @@ class PaymentAPITests(TestCase):
     @patch("borrowings.signals.notify_borrowing_create")
     def setUp(self, mocked_notify, mock_create_session):
         mock_create_session.return_value = SessionStripe(
-            id="fake_session_id", url="https://fake-stripe-url.com"
+            id="fake_session_id",
+            url="https://fake-stripe-url.com"
         )
 
         self.client = APIClient()
@@ -87,7 +111,8 @@ class PaymentViewSetTests(TestCase):
     def setUp(self):
         self.client = APIClient()
         self.user = User.objects.create_user(
-            email="user@test.com", password="test1234"
+            email="user@test.com",
+            password="test1234"
         )
         self.client.force_authenticate(self.user)
         self.view = PaymentViewSet()
@@ -103,3 +128,159 @@ class PaymentViewSetTests(TestCase):
         serializer_class = self.view.get_serializer_class()
 
         self.assertEqual(serializer_class, PaymentRetrieveSerializer)
+
+
+class PaymentSuccessCancelTests(TestCase):
+    @patch("stripe.checkout.Session.create")
+    @patch("borrowings.signals.notify_borrowing_create")
+    def setUp(self, mocked_notify, mock_create_session):
+        mock_create_session.return_value = SessionStripe(
+            id="fake_session_id",
+            url="https://fake-stripe-url.com"
+        )
+
+        self.user = User.objects.create_user(
+            email="test@example.com",
+            password="password123",
+        )
+        self.book = Book.objects.create(
+            title="Test Book",
+            author="Author Name",
+            cover="HARD",
+            inventory=10,
+            daily_fee=5.00,
+        )
+
+        self.borrowing = Borrowing.objects.create(
+            user=self.user,
+            book=self.book,
+            expected_return_date=datetime.date(2024, 12, 12),
+        )
+        self.payment = Payment.objects.create(
+            borrowing=self.borrowing,
+            session_id="test_session_id",
+            status="PAID",
+            money_to_pay=2000
+        )
+
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    @patch("stripe.checkout.Session.retrieve")
+    def test_payment_success(self, mock_stripe_retrieve):
+        mock_stripe_retrieve.return_value = SuccessSessionTest(
+            CustomerDetailsTest(name="Maks", email="test@test.com")
+        )
+
+        response = self.client.get(get_success(self.payment.id))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["customer"]["name"], "Maks")
+        self.assertEqual(response.data["customer"]["email"], "test@test.com")
+        self.assertIn("Thanks for your order", response.data["detail"])
+
+    @patch("stripe.checkout.Session.retrieve")
+    def test_payment_success_no_session(self, mock_stripe_retrieve):
+        self.payment.session_id = None
+        self.payment.save()
+
+        response = self.client.get(get_success(self.payment.id))
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data["detail"], "Session ID is required")
+
+    @patch("stripe.checkout.Session.retrieve")
+    def test_payment_success_stripe_error(self, mock_stripe_retrieve):
+        mock_stripe_retrieve.side_effect = stripe.error.StripeError(
+            "Some error occurred"
+        )
+
+        response = self.client.get(get_success(self.payment.id))
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data["detail"], "Some error occurred")
+
+
+class PaymentCancelTests(TestCase):
+    @patch("stripe.checkout.Session.create")
+    @patch("borrowings.signals.notify_borrowing_create")
+    def setUp(self, mocked_notify, mock_create_session):
+        mock_create_session.return_value = SessionStripe(
+            id="fake_session_id",
+            url="https://fake-stripe-url.com"
+        )
+
+        self.user = User.objects.create_user(
+            email="test@example.com",
+            password="password123",
+        )
+
+        self.book = Book.objects.create(
+            title="Test Book",
+            author="Author Name",
+            cover="HARD",
+            inventory=10,
+            daily_fee=5.00,
+        )
+
+        self.borrowing = Borrowing.objects.create(
+            user=self.user,
+            book=self.book,
+            expected_return_date=datetime.date(2024, 12, 12),
+        )
+        self.payment = Payment.objects.create(
+            borrowing=self.borrowing,
+            session_id="test_session_id",
+            status="PENDING",
+            money_to_pay=2000
+        )
+
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def test_payment_cancel_pending(self):
+        response = self.client.get(get_cancel(self.payment.id))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.data["payment"]["session_id"],
+            "test_session_id"
+        )
+        self.assertIn("Okay,", response.data["detail"])
+
+    def test_payment_cancel_paid(self):
+        self.payment.status = "PAID"
+        self.payment.save()
+
+        response = self.client.get(get_cancel(self.payment.id))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn(
+            f"Dear '{self.user.email}', you have already "
+            f"paid this borrowing!",
+            response.data["detail"]
+        )
+
+    def test_payment_cancel_forbidden(self):
+        other_user = User.objects.create_user(
+            email="otheruser@email.co",
+            password="password123"
+        )
+        self.client.force_authenticate(other_user)
+
+        response = self.client.get(get_cancel(self.payment.id))
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(
+            response.data["detail"],
+            "It`s not your payment! Be more attentive!"
+        )
+
+    def test_payment_cancel_no_session(self):
+        self.payment.session_id = None
+        self.payment.save()
+
+        response = self.client.get(get_cancel(self.payment.id))
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data["detail"], "Session ID is required")
